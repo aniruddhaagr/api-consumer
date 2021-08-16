3892791ecac9285579fb5ae38cf3645ca5d0d62b
Aniruddha Agrawal <aniruddhaagr12@gmail.com>
2021-08-16 23:12:36 +0530

    Added routes

---
+++ src/components/navigation.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
+
+function Navigation(props) {
+  return (
+    <div className="navigation">
+      <nav class="navbar navbar-expand navbar-dark bg-dark">
+        <div class="container">
+          <Link class="navbar-brand" to="/">
+            React Consumer side application
+          </Link>
+
+          <div>
+            <ul class="navbar-nav ml-auto">
+              <li
+                class={`nav-item  ${
+                  props.location.pathname === "/" ? "active" : ""
+                }`}
+              >
+                <Link class="nav-link" to="/">
+                  Home
+                  <span class="sr-only">(current)</span>
+                </Link>
+              </li>
+              <li
+                class={`nav-item  ${
+                  props.location.pathname === "/profile" ? "active" : ""
+                }`}
+              >
+                <Link class="nav-link" to="/profile">
+                  Profile
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default withRouter(Navigation);
